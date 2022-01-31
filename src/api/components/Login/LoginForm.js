57ce7c558d11cfabae527de3b6b17b959ee20558
bmalkes <bmalkes@gmail.com>
2022-01-31 14:54:21 +0200

    feat:login-form

--- src/api/components/Login/LoginForm.js
+++ src/api/components/Login/LoginForm.js
@@ -4,6 +4,10 @@ import useForm from "../../../hooks/useForm";
 import Input from "../Forms/Input.js";
 import Button from "../Forms/Button.js";
 import { UserContext } from "../../../UserContext";
+import Error from "../Helpers/Error";
+
+import styles from "./LoginForm.module.css";
+import stylesBtn from "../../components/Forms/Button.module.css";
 
 function LoginForm() {
     const username = useForm();
@@ -19,8 +23,8 @@ function LoginForm() {
     };
 
     return (
-        <section>
-            <h1>Login</h1>
+        <section className="animeLeft">
+            <h1 className="title">Login</h1>
             <form action="" onSubmit={handleSubmit}>
                 <Input label="User" type="text" name="username" {...username} />
                 <Input
@@ -34,10 +38,18 @@ function LoginForm() {
                 ) : (
                     <Button>Enter</Button>
                 )}
-
-                {error && <p>{error}</p>}
+                <Error error={error} />
             </form>
-            <Link to="/login/create">Sign in</Link>
+            <Link className={styles.perdeu} to="/login/lost">
+                Forgot password?
+            </Link>
+            <div className={styles.signin}>
+                <h2 className={styles.subtitle}>Register</h2>
+                <p>Don't have an account yet ? register on the site</p>
+                <Link className={stylesBtn.button} to="/login/create">
+                    Sign in
+                </Link>
+            </div>
         </section>
     );
 }
