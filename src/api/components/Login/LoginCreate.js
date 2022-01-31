c480b5bb292d512fcc3b471d3c76e2d7fc05dbdb
bmalkes <bmalkes@gmail.com>
2022-01-31 19:41:03 +0200

    feat:login

--- src/api/components/Login/LoginCreate.js
+++ src/api/components/Login/LoginCreate.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useContext } from "react";
+import { USER_POST } from "../../../api";
+import useForm from "../../../hooks/useForm";
+import Button from "../Forms/Button";
+import Input from "../Forms/Input";
+import { UserContext } from "../../../UserContext";
 
 const LoginCreate = () => {
-    return <div>Login Create</div>;
+    const username = useForm();
+    const email = useForm("email");
+    const password = useForm();
+
+    const { userLogin } = useContext(UserContext);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const { url, options } = USER_POST({
+            username: username.value,
+            email: email.value,
+            password: password.value,
+        });
+        const response = await fetch(url, options);
+        console.log(response);
+        if (response.ok) {
+            console.log(response.ok);
+            userLogin(username.value, password.value);
+        }
+    }
+
+    return (
+        <section className="animeLeft">
+            <h1 className="title">Register</h1>
+            <form onSubmit={handleSubmit}>
+                <Input label="User" type="text" name="username" {...username} />
+                <Input label="Email" type="email" name="email" {...email} />
+                <Input
+                    label="Password"
+                    type="password"
+                    name="password"
+                    {...password}
+                />
+                <Button>Register</Button>
+            </form>
+        </section>
+    );
 };
 
 export default LoginCreate;
--- src/api/components/Login/index.js
+++ src/api/components/Login/index.js
@@ -5,19 +5,22 @@ import LoginCreate from "./LoginCreate";
 import LoginPasswordLost from "./LoginPasswordLost";
 import LoginPasswordReset from "./LoginPasswordReset";
 import UserContext from "../../../UserContext";
+import style from "./Login.module.css";
 
 const Login = () => {
     const { login } = useContext(UserContext);
     if (login === true) return <Navigate to="/account" />;
     return (
-        <div>
-            <Routes>
-                <Route path="/" element={<LoginForm />} />
-                <Route path="/create" element={<LoginCreate />} />
-                <Route path="/lost" element={<LoginPasswordLost />} />
-                <Route path="/reset" element={<LoginPasswordReset />} />
-            </Routes>
-        </div>
+        <section className={style.login}>
+            <div className={style.form}>
+                <Routes>
+                    <Route path="/" element={<LoginForm />} />
+                    <Route path="/create" element={<LoginCreate />} />
+                    <Route path="/lost" element={<LoginPasswordLost />} />
+                    <Route path="/reset" element={<LoginPasswordReset />} />
+                </Routes>
+            </div>
+        </section>
     );
 };
 
