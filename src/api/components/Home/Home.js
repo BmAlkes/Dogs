a21f982c98dee8c4a2c8f46ed6b155c853097506
bmalkes <bmalkes@gmail.com>
2022-02-09 12:50:59 +0200

    feat:loading

--- src/api/components/Home/Home.js
+++ src/api/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Feed from "../Feed/Feed";
+import Head from "../Helpers/Head";
+import Loading from "../Helpers/Loading";
 
 const Home = () => {
     return (
         <section className="container mainContainer">
+            <Head title="Photos " />
             <Feed />
         </section>
     );
